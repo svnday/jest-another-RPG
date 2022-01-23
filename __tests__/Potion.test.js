c1c4dfa7ae6bcc76238209b92d37df0588b8cf06
svnday <40030806+svnday@users.noreply.github.com>
2022-01-23 10:49:24 -0600

    adjusted potion feature after reviewing module code snapshot

--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -1,3 +1,12 @@
+const Potion = require('../lib/Potion.js');
+
+test('creates a health potion object', () => {
+    const potion = new Potion('health');
+
+    expect(potion.name).toBe('health');;
+    expect(potion.value).toEqual(expect.any(Number));
+})
+
 test('creates a random potion object', () => {
     const potion = new Potion();
 
@@ -5,16 +14,5 @@ test('creates a random potion object', () => {
     expect(potion.name.length).toBeGreaterThan(0);
     expect(potion.value).toEqual(expect.any(Number));
 });
-
-function Potion(name) {
-    this.types = ['strength', 'agility', 'health'];
-    this.name = name || this.types[Math.floor(Math.random() * this.types.length)];
-
-    if (this.name === 'health') {
-        this.value = Math.floor(Math.random() * 10 + 30);
-    } else {
-        this.value = Math.floor(Math.random() * 5 + 7);
-    }
-}
 
 module.exports = Potion;
